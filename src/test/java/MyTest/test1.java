a334cedc2b2371e7b759bb45e902ad78b9bca8bb
Punam <poonampawar1024@gmail.com>
2022-02-25 13:01:49 +0530

    First Commit

---
+++ src/test/java/MyTest/test1.java
@@ -0,0 +1,19 @@
+package MyTest;
+import org.testng.annotations.Test;
+public class test1 {
+	
+
+	
+	@Test
+	public void Method1()
+	{
+		System.out.println("Test1:Method1");
+	}
+	@Test
+	public void Method2()
+	{
+		System.out.println("Test1:Method2");
+	}
+	}
+
+
---
+++ src/test/java/MyTest/test2.java
@@ -0,0 +1,16 @@
+package MyTest;
+import org.testng.annotations.Test;
+
+public class test2 {
+	@Test
+	public void Method1()
+	{
+		System.out.println("Test2:Method1");
+	}
+	@Test
+	public void Method2()
+	{
+		System.out.println("Test2:Method2");
+	}
+
+}
