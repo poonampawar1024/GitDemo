1cf4f91d91ff1be08eda764a63adb8fb87c6ddf9
Punam <poonampawar1024@gmail.com>
2022-02-25 15:09:49 +0530

    This is updation from GitDemo

--- src/test/java/MyTest/test1.java
+++ src/test/java/MyTest/test1.java
@@ -8,11 +8,13 @@ public class test1 {
 	public void Method1()
 	{
 		System.out.println("Test1:Method1");
+		System.out.println("GitHub testing!!!");
 	}
 	@Test
 	public void Method2()
 	{
 		System.out.println("Test1:Method2");
+		System.out.println("GitHub testing is done. it is just a sample!!!");
 	}
 	}
 
